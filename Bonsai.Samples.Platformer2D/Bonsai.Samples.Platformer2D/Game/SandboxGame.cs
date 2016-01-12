64e82fc1b98d7353815cd3af451784c938d7ed46
james.houden@gmail.com <james.houden@gmail.com>
2016-01-12 22:38:59 +1300

    Added file vs ignored in commit

---
+++ Bonsai.Samples.Platformer2D/Bonsai.Samples.Platformer2D/Game/SandboxGame.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Bonsai.Framework;
+using Bonsai.Framework.Actors;
+using Bonsai.Framework.Common;
+using Bonsai.Framework.Screens;
+using System.Diagnostics;
+using Bonsai.Samples.Platformer2D.Game.Actors;
+using Bonsai.Samples.Platformer2D.Game;
+
+namespace Bonsai.Samples.Platformer2D
+{
+    public class SandboxGame : BonsaiGame
+    {
+        public SandboxGame()
+        {
+            Content.RootDirectory = "Content";
+        }
+
+
+        protected override void Init()
+        {
+            // Setup game window
+            base.SetWindow(width: 800, height: 600, showMouse: true);
+
+            // Create level
+            var level = new Level();
+            level.Exit += () => { this.Exit(); };
+
+            base.GameObjects.Add(level);
+        }
+
+    }
+}
