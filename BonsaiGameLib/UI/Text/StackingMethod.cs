d220fbfdc87e1915c0e7ad1d33e48ab3b7b214a4
James H <james.houden@gmail.com>
2020-08-03 20:48:49 +1200

    rename base folders and proj names

--- BonsaiGameLib/UI/Text/StackingMethod.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Bonsai.Framework.UI.Text
-{
-    public enum StackingMethod { Queue, Parallel }
-}
--- BonsaiGameLib/Variables/GameVariableChangedHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Bonsai.Framework.Variables
-{
-    public delegate void GameVariableChangedHandler<T>(T newValue);
-}
